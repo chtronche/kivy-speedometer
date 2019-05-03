5a5fa63fa35cb262d8641a548aea25bf6dce9771
Ch. Tronche <ch@tronche.com>
2019-05-04 00:31:12 +0200

    label working

--- speedmeter.py
+++ speedmeter.py
@@ -16,18 +16,18 @@ from math import cos, radians, sin
 
 class _X: pass
 
-def _drawOuterCadran(alpha0, alpha1, centerx, centery, r, width):
+def _drawOuterCadran(centerx, centery, r, alpha0, alpha1, width):
     if alpha0 == alpha1:
         Line(circle=(centerx, centery, r), width=width)
     else:
-        ra0 = radians(alpha0 + 90)
-        ra1 = radians(alpha1 + 90)
+        ra0 = radians(alpha0)
+        ra1 = radians(alpha1)
         Line(points=(
-            centerx - r * cos(ra0),
-            centery + r * sin(ra0),
+            centerx + r * sin(ra0),
+            centery + r * cos(ra0),
             centerx, centery,
-            centerx - r * cos(ra1),
-            centery + r * sin(ra1),
+            centerx + r * sin(ra1),
+            centery + r * cos(ra1),
             ),
             width=width,
                  )
@@ -36,37 +36,39 @@ def _drawOuterCadran(alpha0, alpha1, centerx, centery, r, width):
 class SpeedMeter(Widget):
 
     min = NumericProperty(0)
-    tick = NumericProperty(10)
     max = NumericProperty(100)
-    value = NumericProperty(0)
+    
     startAngle = NumericProperty(-90, min=-360,max=360)
     endAngle = NumericProperty(135, min=-360,max=360)
+
+    tick = NumericProperty(10)
     subtick = NumericProperty(0)
-    label = StringProperty('')
     displayFirst = BooleanProperty(True)
     displayLast = BooleanProperty(True)
+
+    label = StringProperty('')
+    labelRadiusRatio = NumericProperty(0.3)
+    labelAngleRatio = NumericProperty(0.5)
+
+    value = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super(SpeedMeter, self).__init__(**kwargs)
         self.a = self.b = 0 # In case on_value is called before _update
         self.rotate = _X
-        self.bind(pos=self._update, size=self._update)
+        self.bind(pos=self._draw, size=self._draw)
 
-    def labelStr(self, n):
+    def valueStr(self, n):
         # Override this if you want more control on the tick display
         return str(int(n))
 
-    def _drawLabels(self, alpha0, alpha1, centerx, centery, r):
-        # For some reason Line(circle=) is rotated 90 degrees ???
-        alpha0 += 90
-        alpha1 += 90
-
-        labelStr = self.labelStr
-        labels = [Label(labelStr(i), bold=True)
+    def _drawValues(self, centerx, centery, r, alpha0, alpha1):
+        valueStr = self.valueStr
+        values = [Label(valueStr(i), bold=True)
                       for i in xrange(self.min, self.max + 1, self.tick)]
-        for _ in labels: _.refresh()
+        for _ in values: _.refresh()
 
-        deltaAlpha = radians((alpha1 - alpha0) / float(len(labels) - 1))
+        deltaAlpha = radians((alpha1 - alpha0) / float(len(values) - 1))
         alpha = radians(alpha0)
         r_10 = r - 10
         r_20 = r - 20
@@ -75,25 +77,23 @@ class SpeedMeter(Widget):
             subDeltaAlpha = deltaAlpha / subtick
         else:
             subDeltaAlpha = None
-        for label in labels:
-            first = label is labels[0]
-            last = label is labels[-1]
+        for value in values:
+            first = value is values[0]
+            last = value is values[-1]
             c = cos(alpha)
             s = sin(alpha)
             if not first and not last or first and self.displayFirst or last and self.displayLast:
                 # Draw the big tick
                 Line(points=(
-                    centerx - r * c, centery + r * s,
-                    centerx - r_10 * c, centery + r_10 * s,
+                    centerx + r * s, centery + r * c,
+                    centerx + r_10 * s, centery + r_10 * c,
                     ),
                     width=2)
                 # Numerical value
-                t = label.texture
+                t = value.texture
                 tw, th = t.size
-                tw /= 2
-                th /= 2
                 Rectangle(
-                    pos=(centerx - r_20 * c - tw, centery + r_20 * s - th),
+                    pos=(centerx + r_20 * s - tw / 2, centery + r_20 * c - th / 2),
                     size=t.size,
                     texture=t)
             # Subtick
@@ -103,22 +103,34 @@ class SpeedMeter(Widget):
                     subc = cos(subAlpha)
                     subs = sin(subAlpha)
                     Line(points=(
-                        centerx - r * subc, centery + r * subs,
-                        centerx - r_10 * subc, centery + r_10 * subs))
+                        centerx + r * subs, centery + r * subc,
+                        centerx + r_10 * subs, centery + r_10 * subc))
                     subAlpha += subDeltaAlpha
             alpha += deltaAlpha
 
     def _drawNeedle(self, centerx, centery, r):
-            Color(0,0,1)
-            if self.value < self.min: self.value = self.min
-            elif self.value > self.max: self.value = self.max
-            self.on_value()
-            needleSize = r
-            s = needleSize * 2
-            Rectangle(pos=(centerx - needleSize, centery - needleSize), size=(s, s),
-                          source='needle.png')
+        Color(0,0,1)
+        if self.value < self.min: self.value = self.min
+        elif self.value > self.max: self.value = self.max
+        self.on_value()
+        needleSize = r
+        s = needleSize * 2
+        Rectangle(pos=(centerx - needleSize, centery - needleSize), size=(s, s),
+                      source='needle.png')
+        
+    def _drawLabel(self, centerx, centery, r, alpha0, alpha1):
+        if not self.label: return
+        l = Label(self.label)
+        l.refresh()
+        t = l.texture
+        tw, th = t.size
+        alpha = self.startAngle + self.labelAngleRatio * (self.endAngle - self.startAngle)
+        c = cos(radians(alpha))
+        s = sin(radians(alpha))
+        r *= self.labelRadiusRatio
+        Rectangle(pos=(centerx + r * s - tw/2, centery + r * c - th/2), size=t.size, texture=t)
 
-    def _update(self, *args):
+    def _draw(self, *args):
         d = min(self.size)
         
         self.canvas.clear()
@@ -134,11 +146,11 @@ class SpeedMeter(Widget):
             
             width = 1.5
 
-            _drawOuterCadran(alpha0, alpha1, centerx, centery, r, width)
+            _drawOuterCadran(centerx, centery, r, alpha0, alpha1, width)
 
             if alpha0 == alpha1:
                 alpha1 = alpha1_ = alpha0 + 360
-            self._drawLabels(alpha0, alpha1, centerx, centery, r)
+            self._drawValues(centerx, centery, r, alpha0, alpha1)
             #
             # Needle (compute parameters and draw)
             #
@@ -149,6 +161,8 @@ class SpeedMeter(Widget):
             self.rotate = Rotate(origin=(centerx, centery))
             self._drawNeedle(centerx, centery, r)
             PopMatrix()
+
+            self._drawLabel(centerx, centery, r, alpha0, alpha1)
 
     def on_value(self, *t):
         self.rotate.angle = self.a * self.value + self.b
