a404d04dedd18c6d0b5ddba951934e7065f4c9f6
Ch. Tronche <ch@tronche.com>
2019-05-18 16:07:55 +0200

    Added getValue and collide_point

--- speedmeterexample2.py
+++ speedmeterexample2.py
@@ -6,6 +6,17 @@ from kivy.app import App
 
 from speedmeter import SpeedMeter
 
-class SpeedMeterExample2(App): pass
+class SpeedMeterExample2(App):
+
+    def on_start(self):
+        self.root.ids['sm'].bind(on_touch_down=self._touch_down)
+
+    def _touch_down(self, sm, motionEvent):
+        if motionEvent.is_double_tap:
+            pass
+        else:
+            v = sm.getValue(motionEvent.pos)
+            if v is None: return
+            sm.value = v
 
 SpeedMeterExample2().run()
