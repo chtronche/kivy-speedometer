83cc248626c47835e9f65d6b4bc32f02d28dffd0
Ch. Tronche <ch@tronche.com>
2019-07-27 16:39:18 +0200

    better refill / speed interaction in demo1

--- demo1.py
+++ demo1.py
@@ -1,5 +1,11 @@
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+
+from math import exp
+
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.factory import Factory
 
 from speedmeter import SpeedMeter
 
@@ -9,13 +15,33 @@ class NoValueSpeedMeter(SpeedMeter):
 
 class Demo1(App):
 
+    def __init__(self):
+        App.__init__(self)
+        self.clockRunning = True
+
     def tick(self, *t, **kw):
         ids = self.root.ids
-        sList = (ids.speed, ids.clock, ids.fuel, ids.temperature, ids.rpm, ids.pi)
-        for s in sList:
-            if s != ids.speed:
-                s.value += 0.2
-                if s.value >= s.max - 0.001: s.value = s.min
+
+        if self.clockRunning:
+            clock = ids.clock
+            clock.value += 0.2
+            if clock.value > clock.max - 0.001: clock.value = clock.min
+
+        fuel = ids.fuel
+        fuel.value -= 0.02 * ids.rpm.value
+        if fuel.value < fuel.min: fuel.value = fuel.min
+
+        speed_value = ids.speed_value
+        if fuel.value <= fuel.min and speed_value.value > speed_value.min:
+            speed_value.value = speed_value.value * 0.9 - 3
+            if speed_value.value <= speed_value.min:
+                speed_value.value = speed_value.min
+                speed_value.disabled = True
+        
+    def set_speed(self):
+        ids = self.root.ids
+        fuel = ids.fuel
+        ids.rpm.value = exp(ids.speed_value.value / 200.0) * 4.5 - 4.5
 
 example = Demo1()
 Clock.schedule_interval(example.tick, 0.5)
